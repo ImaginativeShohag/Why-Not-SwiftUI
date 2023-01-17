98bc093fd383cb90b99a204fc2f280b3650dbf25
Mahmudul Hasan Shohag <mhasan4@apple.com>
2023-01-17 23:49:44 +0600

    feat(NativeAlert): add UIAlertAction.Style parameter, refactor code

--- Why Not SwiftUI/UIs/NativeAlert/Component/NativeAlert.swift
+++ Why Not SwiftUI/UIs/NativeAlert/Component/NativeAlert.swift
@@ -5,6 +5,54 @@
 import Foundation
 import SwiftUI
 
+// MARK: - Extensions
+
+extension UIAlertController {
+    static func createAlert(
+        title: String?,
+        message: String?,
+        primaryButtonText: String,
+        primaryButtonTextColor: Color?,
+        primaryButtonStyle: UIAlertAction.Style,
+        primaryButtonHandler: @escaping ()->Void,
+        secondaryButtonText: String?,
+        secondaryButtonTextColor: Color?,
+        secondaryButtonStyle: UIAlertAction.Style,
+        secondaryButtonHandler: @escaping ()->Void,
+        onDismiss: @escaping ()->Void
+    )->UIAlertController {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+        // MARK: Secondary Button
+
+        if let secondaryButtonText = secondaryButtonText {
+            let secondaryAction = UIAlertAction(title: secondaryButtonText, style: secondaryButtonStyle, handler: { _ in
+                onDismiss()
+                secondaryButtonHandler()
+            })
+            if let secondaryButtonTextColor = secondaryButtonTextColor {
+                secondaryAction.setValue(UIColor(secondaryButtonTextColor), forKey: "titleTextColor")
+            }
+            alert.addAction(secondaryAction)
+        }
+
+        // MARK: Primary Button
+
+        /// We add the primary button in the second place, because it should be on the right of the `Alert`.
+        /// The button placement could be changed based on the "style" parameter by the system.
+        let primaryAction = UIAlertAction(title: primaryButtonText, style: primaryButtonStyle, handler: { _ in
+            onDismiss()
+            primaryButtonHandler()
+        })
+        if let primaryButtonTextColor = primaryButtonTextColor {
+            primaryAction.setValue(UIColor(primaryButtonTextColor), forKey: "titleTextColor")
+        }
+        alert.addAction(primaryAction)
+
+        return alert
+    }
+}
+
 // MARK: - General Approach
 
 private class NativeAlertController: UIViewController {
@@ -12,7 +60,7 @@ private class NativeAlertController: UIViewController {
 
     private var onDismiss: ()->Void
 
-    private let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+    private let alert: UIAlertController
 
     @available(*, unavailable)
     required init?(coder _: NSCoder) { fatalError("") }
@@ -22,39 +70,31 @@ private class NativeAlertController: UIViewController {
         title: String?,
         message: String?,
         primaryButtonText: String,
-        primaryButtonTextColor: Color,
+        primaryButtonTextColor: Color?,
+        primaryButtonStyle: UIAlertAction.Style,
         primaryButtonHandler: @escaping ()->Void,
         secondaryButtonText: String?,
-        secondaryButtonTextColor: Color,
+        secondaryButtonTextColor: Color?,
+        secondaryButtonStyle: UIAlertAction.Style,
         secondaryButtonHandler: @escaping ()->Void,
         onDismiss: @escaping ()->Void
     ) {
         self.isPresented = isPresented
         self.onDismiss = onDismiss
 
-        if let title = title {
-            alert.title = title
-        }
-
-        if let message = message {
-            alert.message = message
-        }
-
-        let primaryAction = UIAlertAction(title: primaryButtonText, style: .default, handler: { _ in
-            onDismiss()
-            primaryButtonHandler()
-        })
-        primaryAction.setValue(UIColor(primaryButtonTextColor), forKey: "titleTextColor")
-        alert.addAction(primaryAction)
-
-        if let secondaryButtonText = secondaryButtonText {
-            let secondaryAction = UIAlertAction(title: secondaryButtonText, style: .default, handler: { _ in
-                onDismiss()
-                secondaryButtonHandler()
-            })
-            secondaryAction.setValue(UIColor(secondaryButtonTextColor), forKey: "titleTextColor")
-            alert.addAction(secondaryAction)
-        }
+        self.alert = UIAlertController.createAlert(
+            title: title,
+            message: message,
+            primaryButtonText: primaryButtonText,
+            primaryButtonTextColor: primaryButtonTextColor,
+            primaryButtonStyle: primaryButtonStyle,
+            primaryButtonHandler: primaryButtonHandler,
+            secondaryButtonText: secondaryButtonText,
+            secondaryButtonTextColor: secondaryButtonTextColor,
+            secondaryButtonStyle: secondaryButtonStyle,
+            secondaryButtonHandler: secondaryButtonHandler,
+            onDismiss: onDismiss
+        )
 
         super.init(nibName: nil, bundle: nil)
     }
@@ -81,10 +121,12 @@ private struct NativeAlert: UIViewControllerRepresentable {
     private let title: String?
     private let message: String?
     private let primaryButtonText: String
-    private let primaryButtonTextColor: Color
+    private let primaryButtonTextColor: Color?
+    private let primaryButtonStyle: UIAlertAction.Style
     private let primaryButtonHandler: ()->Void
     private let secondaryButtonText: String?
-    private let secondaryButtonTextColor: Color
+    private let secondaryButtonTextColor: Color?
+    private let secondaryButtonStyle: UIAlertAction.Style
     private let secondaryButtonHandler: ()->Void
 
     init(
@@ -92,10 +134,12 @@ private struct NativeAlert: UIViewControllerRepresentable {
         title: String?,
         message: String?,
         primaryButtonText: String,
-        primaryButtonTextColor: Color,
+        primaryButtonTextColor: Color?,
+        primaryButtonStyle: UIAlertAction.Style,
         primaryButtonHandler: @escaping ()->Void,
         secondaryButtonText: String?,
-        secondaryButtonTextColor: Color,
+        secondaryButtonTextColor: Color?,
+        secondaryButtonStyle: UIAlertAction.Style,
         secondaryButtonHandler: @escaping ()->Void
     ) {
         self._isPresented = isPresented
@@ -103,9 +147,11 @@ private struct NativeAlert: UIViewControllerRepresentable {
         self.message = message
         self.primaryButtonText = primaryButtonText
         self.primaryButtonTextColor = primaryButtonTextColor
+        self.primaryButtonStyle = primaryButtonStyle
         self.primaryButtonHandler = primaryButtonHandler
         self.secondaryButtonText = secondaryButtonText
         self.secondaryButtonTextColor = secondaryButtonTextColor
+        self.secondaryButtonStyle = secondaryButtonStyle
         self.secondaryButtonHandler = secondaryButtonHandler
     }
 
@@ -116,9 +162,11 @@ private struct NativeAlert: UIViewControllerRepresentable {
             message: message,
             primaryButtonText: primaryButtonText,
             primaryButtonTextColor: primaryButtonTextColor,
+            primaryButtonStyle: primaryButtonStyle,
             primaryButtonHandler: primaryButtonHandler,
             secondaryButtonText: secondaryButtonText,
             secondaryButtonTextColor: secondaryButtonTextColor,
+            secondaryButtonStyle: secondaryButtonStyle,
             secondaryButtonHandler: secondaryButtonHandler,
             onDismiss: onDismiss
         )
@@ -145,64 +193,49 @@ private struct NativeAlert: UIViewControllerRepresentable {
 
 // MARK: - Modern Approach
 
-private class NativeAlertModernController: UIViewController {
+private class NativeAlertModernController<Item: Identifiable>: UIViewController {
+    var item: Item?
+    
     private var onDismiss: ()->Void
-
-    var data: NativeAlertData?
-
     private var alert: UIAlertController?
 
     @available(*, unavailable)
     required init?(coder _: NSCoder) { fatalError("") }
 
     init(
-        data _: NativeAlertData?,
+        item: Item?,
         onDismiss: @escaping ()->Void
     ) {
+        self.item = item
         self.onDismiss = onDismiss
 
         super.init(nibName: nil, bundle: nil)
     }
 
-    private func initAlert(_ data: NativeAlertData)->UIAlertController? {
+    private func createAlert(_ data: NativeAlertData)->UIAlertController {
         alert?.dismiss(animated: true)
 
-        alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+        alert = UIAlertController.createAlert(
+            title: data.title,
+            message: data.message,
+            primaryButtonText: data.primaryButtonText,
+            primaryButtonTextColor: data.primaryButtonTextColor,
+            primaryButtonStyle: data.primaryButtonStyle,
+            primaryButtonHandler: data.primaryButtonHandler,
+            secondaryButtonText: data.secondaryButtonText,
+            secondaryButtonTextColor: data.secondaryButtonTextColor,
+            secondaryButtonStyle: data.secondaryButtonStyle,
+            secondaryButtonHandler: data.secondaryButtonHandler,
+            onDismiss: onDismiss
+        )
 
-        guard let alert = alert else { return nil }
-
-        if let title = data.title {
-            alert.title = title
-        }
-
-        if let message = data.message {
-            alert.message = message
-        }
-
-        let primaryAction = UIAlertAction(title: data.primaryButtonText, style: .default, handler: { _ in
-            self.onDismiss()
-            data.primaryButtonHandler()
-        })
-        primaryAction.setValue(UIColor(data.primaryButtonTextColor), forKey: "titleTextColor")
-        alert.addAction(primaryAction)
-
-        if let secondaryButtonText = data.secondaryButtonText {
-            let secondaryAction = UIAlertAction(title: secondaryButtonText, style: .default, handler: { _ in
-                self.onDismiss()
-                data.secondaryButtonHandler()
-            })
-            secondaryAction.setValue(UIColor(data.secondaryButtonTextColor), forKey: "titleTextColor")
-            alert.addAction(secondaryAction)
-        }
-
-        return alert
+        return alert!
     }
 
     func show(data: NativeAlertData) {
         if alert == nil || alert?.isBeingPresented == false {
-            if let alert = initAlert(data) {
-                present(alert, animated: true, completion: nil)
-            }
+            let alert = createAlert(data)
+            present(alert, animated: true, completion: nil)
         }
     }
 
@@ -226,16 +259,17 @@ private struct NativeAlertModern: UIViewControllerRepresentable {
         self._data = data
     }
 
-    func makeUIViewController(context _: Context)->NativeAlertModernController {
+    func makeUIViewController(context _: Context)->NativeAlertModernController<NativeAlertData> {
         return NativeAlertModernController(
-            data: data,
+            item: data,
             onDismiss: onDismiss
         )
     }
 
-    func updateUIViewController(_ uiViewController: NativeAlertModernController, context _: Context) {
-        if uiViewController.data?.id == data?.id { return }
-        uiViewController.data = data
+    func updateUIViewController(_ uiViewController: NativeAlertModernController<NativeAlertData>, context _: Context) {
+        if uiViewController.item?.id == data?.id { return }
+        
+        uiViewController.item = data
 
         if let data = data {
             uiViewController.show(data: data)
@@ -253,79 +287,6 @@ private struct NativeAlertModern: UIViewControllerRepresentable {
 
 // MARK: - Callback Approach
 
-private class NativeAlertCallbackController<Item: Identifiable>: UIViewController {
-    private var onDismiss: ()->Void
-
-    var item: Item?
-
-    private var alert: UIAlertController?
-
-    @available(*, unavailable)
-    required init?(coder _: NSCoder) { fatalError("") }
-
-    init(
-        item: Item?,
-        onDismiss: @escaping ()->Void
-    ) {
-        self.item = item
-        self.onDismiss = onDismiss
-
-        super.init(nibName: nil, bundle: nil)
-    }
-
-    private func initAlert(_ data: NativeAlertData)->UIAlertController? {
-        alert?.dismiss(animated: true)
-
-        alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
-
-        guard let alert = alert else { return nil }
-
-        if let title = data.title {
-            alert.title = title
-        }
-
-        if let message = data.message {
-            alert.message = message
-        }
-
-        let primaryAction = UIAlertAction(title: data.primaryButtonText, style: .default, handler: { _ in
-            self.onDismiss()
-            data.primaryButtonHandler()
-        })
-        primaryAction.setValue(UIColor(data.primaryButtonTextColor), forKey: "titleTextColor")
-        alert.addAction(primaryAction)
-
-        if let secondaryButtonText = data.secondaryButtonText {
-            let secondaryAction = UIAlertAction(title: secondaryButtonText, style: .default, handler: { _ in
-                self.onDismiss()
-                data.secondaryButtonHandler()
-            })
-            secondaryAction.setValue(UIColor(data.secondaryButtonTextColor), forKey: "titleTextColor")
-            alert.addAction(secondaryAction)
-        }
-
-        return alert
-    }
-
-    func show(data: NativeAlertData) {
-        if alert == nil || alert?.isBeingPresented == false {
-            if let alert = initAlert(data) {
-                present(alert, animated: true, completion: nil)
-            }
-        }
-    }
-
-    func hide() {
-        alert?.dismiss(animated: true) {
-            self.onDismiss()
-        }
-    }
-
-    override func viewWillDisappear(_: Bool) {
-        hide()
-    }
-}
-
 private struct NativeAlertCallback<Item: Identifiable>: UIViewControllerRepresentable {
     @Binding private var item: Item?
     private let content: (Item)->NativeAlertData
@@ -338,14 +299,14 @@ private struct NativeAlertCallback<Item: Identifiable>: UIViewControllerRepresen
         self.content = content
     }
 
-    func makeUIViewController(context _: Context)->NativeAlertCallbackController<Item> {
-        return NativeAlertCallbackController<Item>(
+    func makeUIViewController(context _: Context)->NativeAlertModernController<Item> {
+        return NativeAlertModernController<Item>(
             item: item,
             onDismiss: onDismiss
         )
     }
 
-    func updateUIViewController(_ uiViewController: NativeAlertCallbackController<Item>, context _: Context) {
+    func updateUIViewController(_ uiViewController: NativeAlertModernController<Item>, context _: Context) {
         if uiViewController.item?.id == item?.id { return }
 
         uiViewController.item = item
@@ -372,10 +333,12 @@ extension View {
         title: String? = nil,
         message: String? = nil,
         primaryButtonText: String,
-        primaryButtonTextColor: Color = Color.accentColor,
+        primaryButtonTextColor: Color? = nil,
+        primaryButtonStyle: UIAlertAction.Style = .default,
         primaryButtonHandler: @escaping ()->Void = {},
         secondaryButtonText: String? = nil,
-        secondaryButtonTextColor: Color = Color.accentColor,
+        secondaryButtonTextColor: Color? = nil,
+        secondaryButtonStyle: UIAlertAction.Style = .default,
         secondaryButtonHandler: @escaping ()->Void = {}
     )->some View {
         return AnyView(background(NativeAlert(
@@ -384,9 +347,11 @@ extension View {
             message: message,
             primaryButtonText: primaryButtonText,
             primaryButtonTextColor: primaryButtonTextColor,
+            primaryButtonStyle: primaryButtonStyle,
             primaryButtonHandler: primaryButtonHandler,
             secondaryButtonText: secondaryButtonText,
             secondaryButtonTextColor: secondaryButtonTextColor,
+            secondaryButtonStyle: secondaryButtonStyle,
             secondaryButtonHandler: secondaryButtonHandler
         )))
     }
@@ -418,29 +383,35 @@ struct NativeAlertData: Identifiable {
     let title: String?
     let message: String?
     let primaryButtonText: String
-    let primaryButtonTextColor: Color
+    let primaryButtonTextColor: Color?
+    let primaryButtonStyle: UIAlertAction.Style
     let primaryButtonHandler: ()->Void
     let secondaryButtonText: String?
-    let secondaryButtonTextColor: Color
+    let secondaryButtonTextColor: Color?
+    let secondaryButtonStyle: UIAlertAction.Style
     let secondaryButtonHandler: ()->Void
 
     init(
         title: String? = nil,
         message: String? = nil,
         primaryButtonText: String,
-        primaryButtonTextColor: Color = Color.accentColor,
+        primaryButtonTextColor: Color? = nil,
+        primaryButtonStyle: UIAlertAction.Style = .default,
         primaryButtonHandler: @escaping ()->Void = {},
         secondaryButtonText: String? = nil,
-        secondaryButtonTextColor: Color = Color.accentColor,
+        secondaryButtonTextColor: Color? = nil,
+        secondaryButtonStyle: UIAlertAction.Style = .default,
         secondaryButtonHandler: @escaping ()->Void = {}
     ) {
         self.title = title
         self.message = message
         self.primaryButtonText = primaryButtonText
         self.primaryButtonTextColor = primaryButtonTextColor
+        self.primaryButtonStyle = primaryButtonStyle
         self.primaryButtonHandler = primaryButtonHandler
         self.secondaryButtonText = secondaryButtonText
         self.secondaryButtonTextColor = secondaryButtonTextColor
+        self.secondaryButtonStyle = secondaryButtonStyle
         self.secondaryButtonHandler = secondaryButtonHandler
     }
 }
--- Why Not SwiftUI/UIs/NativeAlert/NativeAlertScreen.swift
+++ Why Not SwiftUI/UIs/NativeAlert/NativeAlertScreen.swift
@@ -7,6 +7,7 @@ import SwiftUI
 struct NativeAlertScreenAlert: Identifiable {
     enum AlertType {
         case create
+        case delete
         case success
     }
 
@@ -19,47 +20,76 @@ struct NativeAlertScreen: View {
     @State var alertType: NativeAlertScreenAlert? = nil
 
     var body: some View {
-        VStack {
+        VStack(spacing: 32) {
             Spacer()
 
-            Button {
-                showAlert = true
-            } label: {
-                Text("Show Alert 1")
+            // MARK: - Variation 1
+
+            VStack {
+                Text("Variation 1")
+                    .font(.system(.title))
+
+                Button {
+                    showAlert = true
+                } label: {
+                    Text("Show Alert")
+                }
             }
 
-            Button {
-                alertData = NativeAlertData(
-                    title: "Alert 2",
-                    primaryButtonText: "Ok"
-                )
-            } label: {
-                Text("Show Alert 2")
+            // MARK: - Variation 2
+
+            VStack {
+                Text("Variation 2")
+                    .font(.system(.title))
+
+                Button {
+                    alertData = NativeAlertData(
+                        title: "You can do it!",
+                        primaryButtonText: "Agreed!"
+                    )
+                } label: {
+                    Text("Show Alert")
+                }
             }
 
-            Button {
-                alertType = NativeAlertScreenAlert(id: .create)
-            } label: {
-                Text("Show Alert 3: Create")
+            // MARK: - Variation 3
+
+            VStack {
+                Text("Variation 3")
+                    .font(.system(.title))
+
+                Button {
+                    alertType = NativeAlertScreenAlert(id: .create)
+                } label: {
+                    Text("Create Alert")
+                }
+
+                Button {
+                    alertType = NativeAlertScreenAlert(id: .delete)
+                } label: {
+                    Text("Delete Alert")
+                }
+
+                Button {
+                    alertType = NativeAlertScreenAlert(id: .success)
+                } label: {
+                    Text("Success Alert")
+                }
             }
 
-            Button {
-                alertType = NativeAlertScreenAlert(id: .success)
-            } label: {
-                Text("Show Alert 3: Success")
-            }
             Spacer()
         }
+        .buttonStyle(.bordered)
         .alert(
             isPresented: $showAlert,
-            title: "Awesome title",
-            message: "Awesome message",
-            primaryButtonText: "Ok",
+            title: "Select Color",
+            message: "Select a color.",
+            primaryButtonText: "Green",
             primaryButtonTextColor: Color(.systemGreen),
             primaryButtonHandler: {
                 //
             },
-            secondaryButtonText: "Cool",
+            secondaryButtonText: "Red",
             secondaryButtonTextColor: Color(.systemRed),
             secondaryButtonHandler: {
                 //
@@ -72,7 +102,17 @@ struct NativeAlertScreen: View {
                 return NativeAlertData(
                     title: "Create?",
                     primaryButtonText: "Yes",
-                    secondaryButtonText: "No"
+                    primaryButtonStyle: .default,
+                    secondaryButtonText: "Cancel",
+                    secondaryButtonStyle: .cancel
+                )
+            case .delete:
+                return NativeAlertData(
+                    title: "Delete?",
+                    primaryButtonText: "Yes",
+                    primaryButtonStyle: .destructive,
+                    secondaryButtonText: "Cancel",
+                    secondaryButtonStyle: .cancel
                 )
             case .success:
                 return NativeAlertData(
