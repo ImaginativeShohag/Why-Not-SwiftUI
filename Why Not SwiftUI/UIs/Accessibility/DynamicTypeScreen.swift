fc400b7c31441946c25c3a5a5e8f91281e16ea52
Md. Mahmudul Hasan Shohag <imaginativeshohag@gmail.com>
2023-09-27 00:21:38 +0600

    Add rotor example, custom action example

--- Why Not SwiftUI/UIs/Accessibility/DynamicTypeScreen.swift
+++ Why Not SwiftUI/UIs/Accessibility/DynamicTypeScreen.swift
@@ -18,11 +18,28 @@ struct DynamicTypeScreen: View {
 struct DynamicTypeScreen_Previews: PreviewProvider {
     static var previews: some View {
         DynamicTypeScreen()
+            .previewDisplayName("Dynamic Type (Medium)")
+            .environment(\.sizeCategory, .medium)
+        
+        DynamicTypeScreen()
+            .previewDisplayName("Dynamic Type (AX1)")
+            .environment(\.sizeCategory, .accessibilityMedium)
     }
 }
 
 /// Layout will be changed based on the Dynamic Type size.
 struct DynamicLayout: View {
+    /// The current Dynamic Type size.
+    ///
+    /// This value changes as the user's chosen Dynamic Type size changes. The
+    /// default value is device-dependent.
+    ///
+    /// When limiting the Dynamic Type size, consider if adding a
+    /// large content view with ``View/accessibilityShowsLargeContentViewer()``
+    /// would be appropriate.
+    ///
+    /// On macOS, this value cannot be changed by users and does not affect the
+    /// text size.
     @Environment(\.dynamicTypeSize) private var dynamicTypeSize
 
     var body: some View {
