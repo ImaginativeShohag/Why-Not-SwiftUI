da2a4ee3ce2099e41c679153f254e1f7ebcb84d8
Md-Saifullah <saif921921@gmail.com>
2025-01-15 01:55:57 +0600

    remove unnecesarry test target

--- packages/TestUtils/Package.swift
+++ packages/TestUtils/Package.swift
@@ -9,16 +9,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TestUtils",
-            targets: ["TestUtils"]),
+            targets: ["TestUtils"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "TestUtils"),
-        .testTarget(
-            name: "TestUtilsTests",
-            dependencies: ["TestUtils"]
-        ),
+        .target(name: "TestUtils"),
     ]
 )
